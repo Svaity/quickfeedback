372e18064f2fbebf6a71ad78825e2ab6dc8b0d56
Shrey Vaity <43662680+Svaity@users.noreply.github.com>
2020-12-03 21:04:06 -0500

    Hello world

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Quick Feedback
+          Flash Feedback
         </h1>
 
         <p className={styles.description}>
